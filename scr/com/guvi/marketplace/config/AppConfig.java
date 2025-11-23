f30587eeb8ace66212c7fa00c528ddb0f9fe17b0
G-SHWETA55 <gshweta13579@gmail.com>
2025-11-24 00:56:45 +0530

    AppConfig
    Updated JDBC_PASSWORD to a placeholder for security.

--- scr/com/guvi/marketplace/config/AppConfig.java
+++ scr/com/guvi/marketplace/config/AppConfig.java
@@ -4,6 +4,7 @@ package com.guvi.marketplace.config;
 public class AppConfig {
     public static final String JDBC_URL = "jdbc:mysql://localhost:3306/greentech?useSSL=false";
     public static final String JDBC_USER = "root";
-    public static final String JDBC_PASSWORD = "13579@";
+    public static final String JDBC_PASSWORD = "ENTER YOUR PASSWORD";
     public static final int POOL_SIZE = 10;
 }
+
